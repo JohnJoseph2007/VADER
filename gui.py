24859b9449295b697f55a0c543386ea27835867a
John <write2johnjoseph@gmail.com>
2022-01-27 08:10:44 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,5 +1,18 @@
 import vader as v
+from tkinter import *
+from tkinter import ttk
 
-n = input("Enter a sentence: ")
-obj = v.Vader(n)
-obj.analyze()
+root = Tk()
+root.title("VADER Sentiment Analyzer")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+sentence = StringVar()
+si = ttk.Entry(mainframe, width=7, textvariable=sentence)
+si.grid(column=2, row=1, sticky=(W, E))
+
+analyze = StringVar()
+ttk.label()
