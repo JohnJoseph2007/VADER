95090f4f304eab55e6a17a15ae1bbbea4be22595
John <write2johnjoseph@gmail.com>
2022-01-27 10:20:24 +0530

    finished the gui

--- gui.py
+++ gui.py
@@ -1,6 +1,14 @@
+from pip import main
 import vader as v
 from tkinter import *
 from tkinter import ttk
+
+def valuereturn():
+    obj = v.Vader(sentence.get())
+    obj.analyze()
+    pos.set(obj.positive*100)
+    neg.set(obj.negative*100)
+    neu.set(obj.neutral*100)
 
 root = Tk()
 root.title("VADER Sentiment Analyzer")
@@ -11,8 +19,23 @@ root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
 sentence = StringVar()
-si = ttk.Entry(mainframe, width=7, textvariable=sentence)
-si.grid(column=2, row=1, sticky=(W, E))
+ttk.Label(mainframe, text="How are you?").grid(column=1, row=1, sticky=(W, E))
+sentenceentry = ttk.Entry(mainframe, width=7, textvariable=sentence)
+sentenceentry.grid(column=2, row=1, sticky=(W, E))
+ttk.Button(mainframe, text="Calculate", command=valuereturn).grid(column=2, row=2, sticky=(W))
 
-analyze = StringVar()
-ttk.label()
+pos = StringVar()
+neg = StringVar()
+neu = StringVar()
+ttk.Label(mainframe, text="How you're feeling:").grid(column=1, row=3, sticky=(W, E))
+ttk.Label(mainframe, textvariable=pos).grid(column=1, row=4, sticky=(W, E))
+ttk.Label(mainframe, textvariable=neu).grid(column=1, row=5, sticky=(W, E))
+ttk.Label(mainframe, textvariable=neg).grid(column=1, row=6, sticky=(W, E))
+
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+sentenceentry.focus()
+root.bind("<Return>", valuereturn)
+
+root.mainloop()
--- vader.py
+++ vader.py
@@ -4,10 +4,17 @@ class Vader:
     def __init__(self, sentence):
         self.sentence = sentence
     
+    positive = 0
+    negative = 0
+    neutral = 0
+    
     def analyze(self):
         sia = SentimentIntensityAnalyzer()
         score = sia.polarity_scores(self.sentence)
-        print("Overall sentiment scores are:\n{0}".format(score))
-        print("Sentence Rating (positive): ", score['pos']*100, "%")
-        print("Sentence Rating (neutral): ", score['neu']*100, "%")
-        print("Sentence Rating (negative): ", score['neg']*100, "%")
+        # print("Overall sentiment scores are:\n{0}".format(score))
+        # print("Sentence Rating (positive): ", score['pos']*100, "%")
+        # print("Sentence Rating (neutral): ", score['neu']*100, "%")
+        # print("Sentence Rating (negative): ", score['neg']*100, "%")
+        self.positive = score['pos']
+        self.negative = score['neg']
+        self.neutral = score['neu']
